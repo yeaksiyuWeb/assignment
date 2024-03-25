d7960de8aa61deef99bd7d975746fb8798af7445
yeaksiyuWeb <syyeakwillie@gmail.com>
2024-03-25 09:11:52 +0800

    migration for admin, course and courseenrolls

--- database/migrations/2024_03_24_150252_create_course_table.php
+++ database/migrations/2024_03_24_150252_create_course_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('course', function (Blueprint $table) {
             $table->id();
+            $table->string('course_code')->nullable();
+            $table->string('course_name')->nullable();
+            $table->string('course_unit')->nullable();
+            $table->integer('no_of_seats')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2024_03_24_150339_create_courseenrolls_table.php
+++ database/migrations/2024_03_24_150339_create_courseenrolls_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('courseenrolls', function (Blueprint $table) {
             $table->id();
+            $table->string('student_reg_no')->nullable;
+            $table->string('pin_code')->nullable;
+            $table->integer('session')->nullable;
+            $table->integer('department')->nullable;
+            $table->integer('level')->nullable;
+            $table->integer('semester')->nullable;
+            $table->integer('course')->nullable;
+            $table->timestamp('enroll_date')->useCurrent();
             $table->timestamps();
         });
     }
