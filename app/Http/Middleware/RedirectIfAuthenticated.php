1b6a52f279192d125243836609633f64c7fa13aa
Czooi <126080176+Czooi@users.noreply.github.com>
2024-03-27 00:01:17 +0800

    update

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Middleware;
+ 
+use Closure;
+use Illuminate\Support\Facades\Auth;
+ 
+class RedirectIfAuthenticated
+{
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+            return redirect('/admin');
+        }
+        if ($guard == "author" && Auth::guard($guard)->check()) {
+            return redirect('/author');
+        }
+        if (Auth::guard($guard)->check()) {
+            return redirect('/home');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('login',[LoginController::class,'showStudentLoginForm']);
+Route::post('login',[LoginController::class,'studentLogin']);
