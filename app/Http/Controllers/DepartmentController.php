4d74482d14283fe10eaf8dbd766998779991172c
yeaksiyuWeb <syyeakwillie@gmail.com>
2024-03-30 22:26:10 +0800

    added DepartmentController.php, department.blade.php, and a route

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    //
+    public function index() {
+
+    }
+
+    public function showDepartmentPage() {
+        return view('admin/department');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DepartmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +24,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::view('/course', '/admin/coursePage');
+Route::get('/department',[DepartmentController::class,'showDepartmentPage']);
 Route::get('/login/admin',[LoginController::class,'showAdminLoginForm']);
 Route::get('/login/student',[LoginController::class,'showStudentLoginForm']);
